40e0dd835a16167cd41ea55600c17bf44dce7e49
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-02-28 23:40:37 -0300

    cleaning files

--- Newbie/base-apparel-coming-soon-master/script.js
+++ Newbie/base-apparel-coming-soon-master/script.js
@@ -1,4 +1,30 @@
-let img = document.getElementById("photo");
+const img = document.getElementById("photo");
+const form = document.querySelector("form");
+const emailInput = document.querySelector("#email");
+const emailErrorMessage = document.querySelector("#email-error-message");
+const iconError = document.getElementById("icon-error");
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (emailInput.value === "") {
+    emailErrorMessage.textContent = "Please enter an email address";
+    emailErrorMessage.style.display = "block";
+    iconError.classList.remove("show-icon");
+  } else if (!isValidEmail(emailInput.value)) {
+    emailErrorMessage.textContent = "Please provide a valid email";
+    emailErrorMessage.style.display = "block";
+    iconError.classList.remove("show-icon");
+  } else {
+    emailErrorMessage.style.display = "none";
+    form.submit();
+  }
+});
 
 function updateImgSrc() {
   if (window.innerWidth <= 375) {
@@ -6,4 +32,4 @@ function updateImgSrc() {
   }
 }
 
-window.addEventListener("resize", updateImgSrc());
+window.addEventListener("resize", updateImgSrc);
