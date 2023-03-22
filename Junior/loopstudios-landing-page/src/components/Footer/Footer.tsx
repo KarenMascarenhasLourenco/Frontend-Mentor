14ec8dac929a73848dae1e1b823531d34d0cf1ba
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-03-22 01:11:24 -0300

    new project

---
+++ Junior/loopstudios-landing-page/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+const Footer = () => {
+  return (
+    <footer className="attribution">
+    Challenge by <a rel='noopener noreferrer' href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://karenmascarenhaslourenco.github.io/">Karen Lourenço</a>.
+  </footer>
+  )
+}
+
+export default Footer
