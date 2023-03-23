03bb06d1ba71d0b5b7a18268763cb8afe1a3b420
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-03-23 17:39:37 -0300

    style fix: infoCard

--- Junior/loopstudios-landing-page/src/components/InfoCard/InfoCard.tsx
+++ Junior/loopstudios-landing-page/src/components/InfoCard/InfoCard.tsx
@@ -4,7 +4,9 @@ import './InfoCard.css';
 const InfoCard = () => {
   return (
     <section className='info-card'>
-      
+      <div className='no-show'>
+        <img src='./images/desktop/image-interactive.jpg' alt='man with a vr glasses'/>
+      </div>
       <div className='info-vr'>
         <h2>The leader in interactive VR</h2>
         <p>Founded in 2011, Loopstudios has been producing world-class virtual reality 
